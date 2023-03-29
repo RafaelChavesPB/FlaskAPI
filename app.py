7156b644f045fef0eefbc038bd30e83860068323
Rafael Cavalcante chaves <rafael.chaves@polodeinovacao.ifpb.edu.br>
2023-03-29 20:24:13 -0300

    Atualização do app

--- app.py
+++ app.py
@@ -5,14 +5,20 @@ import uuid
 
 app = Flask(__name__)
 
+
 @app.get('/store')
 def get_stores():
-    return {"stores": stores}, 200
+    return {"stores": list(stores.values())}, 200
 
 
 @app.post('/store')
 def create_store():
     data = request.get_json()
+    if 'name' not in data:
+        abort(400, message = "Bad request. Ensure 'name' are included in the JSON payload.")
+    for store in stores.values():
+        if store['name'] == data['name']:
+            abort(400, message = "Store already exists.")
     store_id = uuid.uuid1().hex
     new_store = {**data, "id": store_id}
     stores[store_id] = new_store
@@ -21,25 +27,34 @@ def create_store():
 
 @app.get('/store/<string:store_id>')
 def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        return {"message": "Store not found"}, 404
+    if store_id not in stores:
+        return abort(404, message = "Store not found")
+    return stores[store_id]
 
 
 @app.post('/item')
 def create_item():
     data = request.get_json()
+    if 'store_id' not in data or 'name' not in data or 'price' not in data:
+        abort(400, message = "Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.")
+    
     if data['store_id'] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message = "Store not found")
+
+    for item in items.values():
+        if item['name'] == data['name'] and item['store_id'] == data['store_id']:
+            abort(400, message = "Item already exists.")
+
     item_id = uuid.uuid1().hex
     new_item = {**data, "id": item_id}
     items[item_id] = new_item
-    return new_item
+    return new_item, 201
+
 
 @app.get('/item')
 def get_items():
-    return items, 200
+    return {"items": list(items.values())}, 200
+
 
 @app.get('/item/<string:item_id>')
 def get_item(item_id):
